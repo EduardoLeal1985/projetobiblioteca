ca013f414be67c6c9fca5a06c8fcfbefaa4d608b
EduardoLeal1985 <eduleal2005@gmail.com>
2019-11-28 16:25:48 -0300

    Tela Inicial versão 1

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import { Container, Form, SubmitButton } from './styles';
+import {
+  Container,
+  Form,
+  SubmitButton1,
+  SubmitButton2,
+  SubmitButton3,
+  SubmitButton4,
+  Text,
+  LogoEntrada,
+} from './styles';
 
 export default class Main extends Component {
   static navigationOptions = {
-    title: <Icon name="home" size={20} color="#000" />,
+    title: 'Home',
   };
 
   render() {
     return (
       <Container>
+        <LogoEntrada
+          source={{
+            uri:
+              'https://image.freepik.com/free-vector/education-book-logo_7492-201.jpg',
+          }}
+        />
         <Form>
-          <SubmitButton>
-            <Icon name="add" size={20} color="#000" />
-          </SubmitButton>
+          <SubmitButton1>
+            <Icon name="book-plus" size={40} color="#333" />
+            <Text>Emprestar</Text>
+          </SubmitButton1>
+          <SubmitButton2>
+            <Icon name="reply" size={40} color="#333" />
+            <Text>Devolver</Text>
+          </SubmitButton2>
+          <SubmitButton3>
+            <Icon name="library-plus" size={40} color="#333" />
+            <Text>Cadastrar Livros</Text>
+          </SubmitButton3>
+          <SubmitButton4>
+            <Icon name="account-plus" size={40} color="#333" />
+            <Text>Cadastrar Usuários</Text>
+          </SubmitButton4>
         </Form>
       </Container>
     );
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -4,19 +4,73 @@ import { RectButton } from 'react-native-gesture-handler';
 export const Container = styled.View`
   flex: 1;
   padding: 30px;
+  align-items: center;
+  background-color: #fff;
+`;
+
+export const LogoEntrada = styled.Image`
+  width: 250px;
+  height: 250px;
 `;
 
 export const Form = styled.View`
-  background-color: #ccc;
+  flex: 1;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  background-color: #fff;
 `;
 
-export const SubmitButton = styled(RectButton)`
+export const SubmitButton1 = styled(RectButton)`
   justify-content: center;
   align-items: center;
-  background: #cecece;
+  background: #ccff00;
   border-radius: 4px;
   margin-left: 10px;
+  margin-top: 10px;
   padding: 0 12px;
+  width: 125px;
+  height: 125px;
 `;
 
-export const Icon = styled.View``;
+export const SubmitButton2 = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #fcba03;
+  border-radius: 4px;
+  margin-left: 10px;
+  margin-top: 10px;
+  padding: 0 12px;
+  width: 125px;
+  height: 125px;
+`;
+
+export const SubmitButton3 = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #bf8fd9;
+  border-radius: 4px;
+  margin-left: 10px;
+  margin-top: 10px;
+  padding: 0 12px;
+  width: 125px;
+  height: 125px;
+`;
+
+export const SubmitButton4 = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #a1bfe3;
+  border-radius: 4px;
+  margin-left: 10px;
+  margin-top: 10px;
+  padding: 0 12px;
+  width: 125px;
+  height: 125px;
+`;
+
+export const Text = styled.Text`
+  color: #333;
+  font-family: 'Roboto';
+  margin-top: 10px;
+`;
