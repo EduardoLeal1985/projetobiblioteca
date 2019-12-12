4254a56a76a22473d9e1b2ed8bd847b638fccadf
EduardoLeal1985 <eduleal2005@gmail.com>
2019-12-12 16:18:10 -0300

    Implementando Página Emprestimos

--- src/pages/Emprestimo/index.js
+++ src/pages/Emprestimo/index.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import {} from 'react-native-camera';
+// import { View } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import { Container, Text, Form, CameraButton } from './styles';
+import {
+  Container,
+  Form,
+  CameraButton,
+  Input,
+  Container2,
+  SubmitButton,
+  SearchButtonText,
+} from './styles';
 
 export default class Emprestimo extends Component {
   static navigationOptions = {
     title: 'Empréstimo',
   };
 
+  handleSearchBook = pesquisa => {
+    console.tron.log(pesquisa);
+  };
+
   render() {
     return (
       <Container>
         <Form>
-          <View>
-            <Text>Página de empréstimo</Text>
-          </View>
-          <CameraButton>
-            <Icon name="camera" size={40} color="#333" />
-          </CameraButton>
+          <Container2>
+            <CameraButton>
+              <Icon name="camera" size={40} color="#777" />
+            </CameraButton>
+            <Input
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Pesquisar por título ou autor"
+              returnKeyType="send"
+              onSubmitEditing={this.handleSearchBook}
+            />
+            <SubmitButton
+              onPress={() => {
+                this.handleSearchBook();
+              }}
+            >
+              <Icon name="magnify" size={20} color="#FFF" />
+              <SearchButtonText>Pesquisar</SearchButtonText>
+            </SubmitButton>
+          </Container2>
         </Form>
       </Container>
     );
--- src/pages/Emprestimo/styles.js
+++ src/pages/Emprestimo/styles.js
@@ -4,30 +4,57 @@ import { RectButton } from 'react-native-gesture-handler';
 export const Container = styled.View`
   flex: 1;
   padding: 30px;
-  align-items: center;
-  background-color: #fff;
 `;
 
-export const Text = styled.Text`
-  color: #333;
-  font-family: 'Roboto';
-  margin-top: 10px;
-`;
-
-export const CameraButton = styled(RectButton)`
-  flex: 1;
+export const CameraButton = styled.TouchableOpacity`
   width: 80px;
   height: 80px;
   border-radius: 40px;
-  border: 1px solid black;
-  background-color: #ccc;
-  text-align: center;
+  background-color: #eee;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Form = styled.View`
-  flex: 1;
   flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  background-color: #fff;
+  padding-bottom: 10px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  width: 95%;
+  height: 40px;
+  background: #eee;
+  border-radius: 4px;
+  padding: 0 15px;
+  border: 1px solid #eee;
+  margin-top: 10px;
+`;
+
+export const Container2 = styled.View`
+  align-items: center;
+  margin: 0 20px 30px;
+  flex: 1;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  margin-top: 10px;
+  border-radius: 4px;
+  background: #999;
+  justify-content: center;
+  align-items: center;
+  height: 36px;
+  width: 95%;
+  flex-direction: row;
+`;
+
+export const SearchButtonText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #fff;
+  text-transform: uppercase;
+  margin-left: 5px;
 `;
